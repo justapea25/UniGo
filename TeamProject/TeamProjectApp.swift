184ac4ba90e3b59e52192bc40f86c74a6abd9dc5
Minh <dauhoangminh9@gmail.com>
2023-07-10 13:30:23 +1000

    Initial Commit

---
+++ TeamProject/TeamProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamProjectApp.swift
+//  TeamProject
+//
+//  Created by Dau Hoang Minh on 10/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct TeamProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
